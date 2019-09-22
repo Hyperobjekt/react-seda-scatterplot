0dac30e159c4846a7b9f863551b123e9c5f9164e
A Groshek <amy@amygroshek.com>
2019-09-22 17:21:58 -0500

    Attempts to add additional transpiling rules

--- nwb.config.js
+++ nwb.config.js
@@ -8,5 +8,17 @@ module.exports = {
         react: 'React'
       }
     }
+  },
+  babel: {
+    cherryPick: ['babel-plugin-transform-parameters', 'babel-plugin-transform-spread', 'babel-plugin-transform-destructuring', 'plugin-transform-arrow-functions'],
+    env: {
+      targets: {
+        browsers: "> 0.5%, not dead",
+        ie: "11"
+      },
+      modules: 'umd'
+    },
+    runtime: "polyfill",
+    stage: 0
   }
 }
